c81f95a2d9162b6ef634aff461039b12fe3d81c8
huy <wywywyz123@gmail.com>
2026-03-31 20:03:21 +0700

    API lay danh sach Giangvien va them Giangvien moi

---
+++ BackEnd/DiemDanhLopHoc/DiemDanhLopHoc/DTOs/NguoiDungDTOs.cs
@@ -0,0 +1,21 @@
+﻿namespace DiemDanhLopHoc.DTOs
+{
+    public class NguoiDungDTOs
+    {
+        public class GiangVienCreateRequest
+        {
+            public string MaGv { get; set; }
+            public string TaiKhoan { get; set; }
+            public string MatKhau { get; set; }
+            public string HoTen { get; set; }
+        }
+        public class GiangVienResponse
+        {
+            public string MaGv { get; set; }
+            public string TaiKhoan { get; set; }
+            public string HoTen { get; set; }
+            public bool? TrangThai { get; set; }
+            //  không để MatKhau 
+        }
+    }
+}
